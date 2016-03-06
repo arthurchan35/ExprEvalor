7cf3b1bbb86a9aba315cdfa5f57ba4d19a985789
arthurchan35 <arthurchan35@gmail.com>
2016-03-06 14:22:50 -0500

    JavaScript and Python
    JavaScript and Python

---
+++ JsExprEvalor/src/JsExprEvalor.js
@@ -0,0 +1,170 @@
+function JsExprEvalor(y_expr, x_val) {
+	this.y_expr = y_expr;
+	this.x_val = x_val;
+	this.index = 0;
+	this.length = y_expr.length;
+	this.parenthesis = new Array();	
+	
+	this.expr = function expr() {
+		var result = this.term();
+
+		while (this.index < this.length && (this.y_expr.charAt(this.index) == "+" || this.y_expr.charAt(this.index) == "-") ) {
+			if (y_expr.charAt(this.index++) == '+') {
+				result += this.term();
+			}
+			else {
+				result -= this.term();
+			}
+		}
+		return result;		
+	}
+
+	this.term = function term() {
+		var result = this.pow();
+
+		while (this.index < this.length && (this.y_expr.charAt(this.index) == "*" || this.y_expr.charAt(this.index) == "/") ) {
+			if (y_expr.charAt(this.index++) == '*') {
+				result *= this.pow();
+			}
+			else {
+				var Divisor = this.pow();
+				if (Divisor == 0) {
+					console.log("Divisor cannot be zero");
+					exit();
+				}
+				result /= Divisor;
+				
+			}
+		}
+		return result;
+	}
+
+	this.pow = function pow() {
+		var result = this.factor();
+
+		while (this.index < this.length && (this.y_expr.charAt(this.index) == "^")) {
+			this.index++;
+			result = Math.pow(result, this.factor());
+		}
+		return result;
+	}
+
+	this.wsSkip = function wsSkip() {
+		while (this.index < this.length && (this.y_expr.charAt(this.index) == ' ' || this.y_expr.charAt(this.index) == '\t'))
+			this.index++;
+
+	}
+
+	this.parenthOp = function parenthOp() {
+		this.index++;
+		this.parenthesis.push("(");
+		var result = this.expr();
+		this.wsSkip();
+
+		if (this.index == this.length || this.y_expr.charAt(this.index) != ")" || this.parenthesis.length == 0 || this.parenthesis.pop() != "(") {
+			console.log("Incorrect closing parenthesis at index " + this.index);
+			exit();
+		}
+
+		this.index++;
+		this.wsSkip();
+		return result;
+
+	}
+
+	this.parenthOp2 = function parenthOp2() {
+		var result = new Array();
+		this.index++;
+		this.parenthesis.push("(");
+		result[0] = this.expr();
+		this.wsSkip();
+
+		if (this.index == this.length || this.y_expr.charAt(this.index) != ",") {
+			console.log("Missing comma at index " + this.index);
+			exit();
+		}
+
+		this.index++;
+		this.wsSkip();
+		result[1] = expr();
+		this.wsSkip();
+
+		if (this.index == this.length || this.y_expr.charAt(this.index) != ")" || this.parenthesis.length == 0 || this.parenthesis.pop() != "(") {
+			console.log("Incorrect closing at index " + this.index);
+			exit();
+		}
+
+		this.index++;
+		this.wsSkip();
+		return result;
+	}
+
+	this.factor = function factor() {
+		this.wsSkip();
+
+		if (this.y_expr.charAt(this.index) == 'x') {
+			this.index++;
+			return this.x_val;
+		}
+
+		if (this.y_expr.charAt(this.index) == 's' && this.y_expr.charAt(this.index + 1) == 'i' && this.y_expr.charAt(this.index + 2) == 'n' && this.y_expr.charAt(this.index + 3) == '(') {
+			this.index += 3;
+			var result = this.parenthOp();
+			return Math.sin(result);
+		}
+
+		if (this.y_expr.charAt(this.index) == 'c' && this.y_expr.charAt(this.index + 1) == 'o' && this.y_expr.charAt(this.index + 2) == 's' && this.y_expr.charAt(this.index + 3) == '(') {
+			this.index += 3;
+			var result = this.parenthOp();
+			return Math.cos(result);
+		}
+
+		if (this.y_expr.charAt(this.index) == 't' && this.y_expr.charAt(this.index + 1) == 'a' && this.y_expr.charAt(this.index + 2) == 'n' && this.y_expr.charAt(this.index + 3) == '(') {
+			this.index += 3;
+			var result = this.parenthOp();
+			return Math.tan(result);
+		}
+
+		if (this.y_expr.charAt(this.index) == 'l' && this.y_expr.charAt(this.index + 1) == 'n' && this.y_expr.charAt(this.index + 2) == '(') {
+			this.index += 2;
+			var result = this.parenthOp();
+			return Math.log(result);
+		}
+
+		if (this.y_expr.charAt(this.index) == 'a' && this.y_expr.charAt(this.index + 1) == 't' && this.y_expr.charAt(this.index + 2) == 'a' && this.y_expr.charAt(this.index + 3) == 'n' && this.y_expr.charAt(this.index + 4) == '2' && this.y_expr.charAt(this.index + 5) == '(') {
+			this.index += 5;
+			var result = this.parenthOp2();
+			return Math.atan2(result[0], result[1]);
+		}
+
+		if (this.y_expr.charAt(this.index) == 'a' && this.y_expr.charAt(this.index + 1) == 't' && this.y_expr.charAt(this.index + 2) == 'a' && this.y_expr.charAt(this.index + 3) == 'n' && this.y_expr.charAt(this.index + 4) == '(') {
+			this.index += 4;
+			var result = this.parenthOp();
+			return Math.atan(result);
+		}
+
+
+		if (this.y_expr.charAt(this.index) == '(')
+			return this.parenthOp();
+
+		var num = "";
+		var result = 0.0;
+
+		while (this.index < this.length && ((this.y_expr.charAt(this.index) <= '9' && this.y_expr.charAt(this.index) >= '0') || this.y_expr.charAt(this.index) == '.'))
+			num += this.y_expr.charAt(this.index++);
+
+		result = parseFloat(num);
+
+		this.wsSkip();
+
+		return result;
+	}
+
+
+
+}
+
+
+var myEvalor = new JsExprEvalor("(2+x)*sin(x/4)", 3.1415926);
+
+document.write(myEvalor.expr());
--- PythonExprEvalor/src/PythonExprEvalor.py
+++ PythonExprEvalor/src/PythonExprEvalor.py
@@ -1,1 +1,171 @@
-print "hello world"
+
+#Created by arthurchan35 on 2/25/2016.
+
+#<expr> : <term> {(+ | -)<term>}
+#<term> : <factor> {(* | /)<term>}
+#<factor> : ID | INT_CONST | ( <expr> )
+import sys
+
+class PythonExprEvalor:
+	#y_expr
+	#x_val
+	#index
+	#length
+	#parenthesis
+
+	def __init__(self, y_expr, x_val):
+		self.y_expr = str(y_expr)
+		self.x_val = float(x_val)
+		self.index = 0
+		self.length = len(str(y_expr))
+		self.parenthesis = []
+
+	def factor(self):
+		self.wsSkip()
+
+		if (self.y_expr[self.index] == 'x') :
+			self.index += 1
+			return self.x_val
+
+		if (self.y_expr[self.index] == 's' and self.y_expr[self.index + 1] == 'i' and
+			self.y_expr[self.index + 2] == 'n' and self.y_expr[self.index + 3] == '('):
+			self.index += 3
+			result = parenthOp()
+			return math.sin(result)
+
+		if (self.y_expr[self.index] == 'c' and self.y_expr[self.index + 1] == 'o' and
+			self.y_expr[self.index + 2] == 's' and self.y_expr[self.index + 3] == '('):
+			self.index += 3
+			result = parenthOp()
+			return math.cos(result)		
+
+		if (self.y_expr[self.index] == 't' and self.y_expr[self.index + 1] == 'a' and
+			self.y_expr[self.index + 2] == 'n' and self.y_expr[self.index + 3] == '('):
+			self.index += 3
+			result = parenthOp()
+			return math.tan(result)
+
+		if (self.y_expr[self.index] == 'l' and self.y_expr[self.index + 1] == 'n' and
+			self.y_expr[self.index + 2] == '('):
+			self.index += 2
+			result = parenthOp()
+			return math.log(result)
+
+		if (self.y_expr[self.index] == 'a' and self.y_expr[self.index + 1] == 't' and
+			self.y_expr[self.index + 2] == 'a' and self.y_expr[self.index + 3] == 'n' and
+			self.y_expr[self.index + 4] == '2' and self.y_expr[self.index + 5] == '('):
+			self.index += 5
+			result = parenthOp2()
+			return math.atan2(result[0], result[1])
+
+		if (self.y_expr[self.index] == 'a' and self.y_expr[self.index + 1] == 't' and
+			self.y_expr[self.index + 2] == 'a' and self.y_expr[self.index + 3] == 'n' and
+			self.y_expr[self.index + 4] == '('):
+			self.index += 4
+			result = parenthOp()
+			return math.atan(result)
+
+		if (self.y_expr[self.index] == '('):
+			return parenthOp()
+
+		num = ""
+		result = 0.0
+
+		while (self.index < self.length and ((self.y_expr[self.index] <= '9' and 
+			self.y_expr[self.index] >= '0') or self.y_expr[self.index] == '.')):
+			num += self.y_expr[self.index]
+			self.index += 1
+
+		try:	
+			result = float(num)
+		except ValueError:
+			print "not a value"
+			sys.exit(-1)
+
+		self.wsSkip()
+		return result
+
+	def parenthOp2(self):
+		result = []
+		self.index += 1
+		self.parenthesis.append('(')
+		result.append(expr())
+		self.wsSkip()
+		if (self.index == self.length or self.y_expr[self.index] != ','):
+			sys.exit(-1)
+
+		self.index += 1
+		self.wsSkip()
+		result.append(expr())
+		self.wsSkip()
+		if (self.index == self.length or self.y_expr[self.index] != ')' or
+			not self.parenthesis or self.parenthesis.pop() != '(') :
+			sys.exit(-1)
+
+		self.index += 1
+		self.wsSkip()
+		return result
+
+	def parenthOp(self):
+		self.index += 1
+		self.parenthesis.append('(')
+		result = self.expr()
+		self.wsSkip()
+		if (self.index == self.length or self.y_expr[self.index] != ')' or
+			not self.parenthesis or self.parenthesis.pop() != '(') : 
+			sys.exit(-1)
+
+		self.index += 1
+		self.wsSkip()
+		return result
+
+	def wsSkip(self):
+		while (self.index < self.length and (self.y_expr[self.index] == ' ' or 
+			self.y_expr[self.index] == '\t')):
+			self.index += 1
+
+	def pow(self):
+		result = self.factor()
+		while (self.index < self.length and self.y_expr[self.index] == '^'):
+			self.index += 1
+			result = math.pow(result, self.factor())
+		return result
+
+	def term(self): 
+		result = self.pow()
+		while (self.index < self.length and (self.y_expr[self.index] == '*' or self.y_expr[self.index] == '/')):
+			if (self.y_expr[self.index] == '*'):
+				result *= self.pow()
+			else:
+				try:
+					result /= self.pow()
+				except ValueError:
+					print "divided by zero"
+					sys.exit(-1)
+			self.index += 1
+		return result			
+
+	def expr(self):
+		result = self.term()
+		while (self.index < self.length and (self.y_expr[self.index] == '+' or self.y_expr[self.index] == '-')):
+			if (self.y_expr[self.index] == '+'):
+				result += self.term()
+			else:
+				result -= self.term()
+			self.index += 1
+		return result
+
+while (True):	
+	y_expr = input('Y = ? ')
+	x_val = input('X ? ')
+
+	pee = PythonExprEvalor(y_expr, x_val)
+	result = pee.expr()
+	if (not pee.parenthesis):
+		print result
+	else:
+		print "Too few closing parenthesis in expression."
+		print len(pee.parenthesis)
+		print pee.parenthesis
+		sys.exit(-1)
+
